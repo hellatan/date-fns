dcfea7178490c08c0410b8a9edd99bdfd29771d5
Lesha Koss <regiusprod@gmail.com>
2017-05-26 20:26:27 +0600

    Improve listLocales

--- scripts/_lib/listLocales.js
+++ scripts/_lib/listLocales.js
@@ -1,14 +1,16 @@
 import path from 'path'
 import fs from 'fs'
 
+const ignoredFiles = ['index.js', 'test.js']
+
 export default function listLocales () {
   const locales = fs.readdirSync(path.join(process.cwd(), 'src', 'locale'))
   return locales
-    .filter((locale) => /^[^._]/.test(locale))
+    .filter((file) => /^[^._]/.test(file) && !ignoredFiles.includes(file))
     .map((locale) => ({
-      name: `${locale}Locale`,
+      name: locale.replace(/-/g, ''),
       code: locale,
-      path: `./locale/${locale}`,
+      path: `./${locale}`,
       fullPath: `./src/locale/${locale}/index.js`
     }))
 }
