60aac1bde2c2eb9d022a7e12b322687476bae90d
Sasha Koss <koss@nocorp.me>
2017-05-29 17:47:49 +0300

    WIP

--- scripts/test/travis.sh
+++ scripts/test/travis.sh
@@ -6,22 +6,33 @@
 
 set -e
 
+export PATH="$(npm bin):$PATH"
+
+function prebuild {
+  env BUILD_TESTS=true webpack --config ./config/webpack.js
+}
+
 if [ "$TEST_SUITE" == "main" ]
 then
   yarn lint
-  yarn flow-check
+  flow check
   ./scripts/test/systemJS.js
   ./scripts/test/smoke.sh
-  yarn test-ci
-  yarn test-tz
+
+  yarn test -- --single-run
+
+  prebuild
+  ./scripts/test/tz.sh
 
 elif [ "$TEST_SUITE" == "tz" ]
 then
-  yarn test-tz-extended
+  prebuild
+  ./scripts/test/tzExtended.sh
 
 elif [ "$TEST_SUITE" == "cross_browser" ] && [ "$SAUCE_USERNAME" != "" ]
 then
   yarn test-cross-browser
+  env TEST_CROSS_BROWSER=true yarn test -- --single-run
 
 else
   printf "\n\033[0;31m" "UNKNOWN SUITE!" "\033[0m\n"
